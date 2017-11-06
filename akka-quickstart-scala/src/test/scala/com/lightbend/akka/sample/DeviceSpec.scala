1c7588f1b49a6e9241be911c83a387b7b2794a49
PhilipL <Philip.Liberato@concur.com>
2017-11-06 14:37:31 -0500

    Part 4: Working with Device Groups

--- akka-quickstart-scala/src/test/scala/com/lightbend/akka/sample/DeviceSpec.scala
+++ akka-quickstart-scala/src/test/scala/com/lightbend/akka/sample/DeviceSpec.scala
@@ -2,12 +2,13 @@ package com.lightbend.akka.sample
 
 import akka.actor.ActorSystem
 import akka.testkit.{TestKit, TestProbe}
-import org.scalatest.{BeforeAndAfterAll, FlatSpecLike, Matchers}
+import org.scalatest.{BeforeAndAfterAll, WordSpecLike, Matchers}
+import scala.concurrent.duration._
 
 class DeviceSpec(_system: ActorSystem)
   extends TestKit(_system)
     with Matchers
-    with FlatSpecLike
+    with WordSpecLike
     with BeforeAndAfterAll {
 
   def this() = this(ActorSystem("DeviceSpec"))
@@ -16,34 +17,56 @@ class DeviceSpec(_system: ActorSystem)
     shutdown(system)
   }
 
-  "A Device Actor" should "reply with empty reading if no temperature is known" in {
-    val probe = TestProbe()
-    val deviceActor = system.actorOf(Device.props("group", "device"))
+  "A Device Actor" should {
+    "reply to registration requests" in {
+      val probe = TestProbe()
+      val deviceActor = system.actorOf(Device.props("group", "device"))
 
-    deviceActor.tell(Device.ReadTemperature(requestId = 42), probe.ref)
-    val response = probe.expectMsgType[Device.RespondTemperature]
-    response.requestId should ===(42)
-    response.value should ===(None)
-  }
+      deviceActor.tell(DeviceManager.RequestTrackDevice("group", "device"), probe.ref)
+      probe.expectMsg(DeviceManager.DeviceRegistered)
+      probe.lastSender should ===(deviceActor)
+    }
 
-  "A Device Actor" should "reply with latest temperature reading" in {
-    val probe = TestProbe()
-    val deviceActor = system.actorOf(Device.props("group", "device"))
+    "ignore wrong registration requests" in {
+      val probe = TestProbe()
+      val deviceActor = system.actorOf(Device.props("group", "device"))
 
-    deviceActor.tell(Device.RecordTemperature(requestId = 1, 24.0), probe.ref)
-    probe.expectMsg(Device.TemperatureRecorded(requestId = 1))
+      deviceActor.tell(DeviceManager.RequestTrackDevice("wrongGroup", "device"), probe.ref)
+      probe.expectNoMsg(500.milliseconds)
 
-    deviceActor.tell(Device.ReadTemperature(requestId = 2), probe.ref)
-    val response1 = probe.expectMsgType[Device.RespondTemperature]
-    response1.requestId should ===(2)
-    response1.value should ===(Some(24.0))
+      deviceActor.tell(DeviceManager.RequestTrackDevice("group", "Wrongdevice"), probe.ref)
+      probe.expectNoMsg(500.milliseconds)
+    }
 
-    deviceActor.tell(Device.RecordTemperature(requestId = 3, 55.0), probe.ref)
-    probe.expectMsg(Device.TemperatureRecorded(requestId = 3))
+    "reply with empty reading if no temperature is known" in {
+      val probe = TestProbe()
+      val deviceActor = system.actorOf(Device.props("group", "device"))
 
-    deviceActor.tell(Device.ReadTemperature(requestId = 4), probe.ref)
-    val response2 = probe.expectMsgType[Device.RespondTemperature]
-    response2.requestId should ===(4)
-    response2.value should ===(Some(55.0))
+      deviceActor.tell(Device.ReadTemperature(requestId = 42), probe.ref)
+      val response = probe.expectMsgType[Device.RespondTemperature]
+      response.requestId should ===(42)
+      response.value should ===(None)
+    }
+
+    "reply with latest temperature reading" in {
+      val probe = TestProbe()
+      val deviceActor = system.actorOf(Device.props("group", "device"))
+
+      deviceActor.tell(Device.RecordTemperature(requestId = 1, 24.0), probe.ref)
+      probe.expectMsg(Device.TemperatureRecorded(requestId = 1))
+
+      deviceActor.tell(Device.ReadTemperature(requestId = 2), probe.ref)
+      val response1 = probe.expectMsgType[Device.RespondTemperature]
+      response1.requestId should ===(2)
+      response1.value should ===(Some(24.0))
+
+      deviceActor.tell(Device.RecordTemperature(requestId = 3, 55.0), probe.ref)
+      probe.expectMsg(Device.TemperatureRecorded(requestId = 3))
+
+      deviceActor.tell(Device.ReadTemperature(requestId = 4), probe.ref)
+      val response2 = probe.expectMsgType[Device.RespondTemperature]
+      response2.requestId should ===(4)
+      response2.value should ===(Some(55.0))
+    }
   }
 }
